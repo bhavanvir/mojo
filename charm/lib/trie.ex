a4681fc1f2c6164b906ac2c39a43cf5bd787be9c
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-03-12 22:39:44 -0700

    Update naming convention of private recursive functions

--- charm/lib/trie.ex
+++ charm/lib/trie.ex
@@ -6,32 +6,32 @@ defmodule Trie do
   defstruct root: %TrieNode{}
 
   def insert(%Trie{root: root} = trie, word) do
-    new_root = do_insert(root, String.graphemes(word))
+    new_root = _insert(root, String.graphemes(word))
     %Trie{trie | root: new_root}
   end
 
-  defp do_insert(node, []), do: %{node | word_end?: true}
+  defp _insert(node, []), do: %{node | word_end?: true}
 
-  defp do_insert(node, [char | rest]) do
+  defp _insert(node, [char | rest]) do
     child = Map.get(node.children, char, %TrieNode{})
-    updated_child = do_insert(child, rest)
+    updated_child = _insert(child, rest)
     updated_children = Map.put(node.children, char, updated_child)
     %{node | children: updated_children}
   end
 
   def search(%Trie{root: root}, word) do
-    do_search(root, String.graphemes(word))
+    _search(root, String.graphemes(word))
   end
 
-  defp do_search(node, []), do: node.word_end?
+  defp _search(node, []), do: node.word_end?
 
-  defp do_search(node, [char | rest]) do
+  defp _search(node, [char | rest]) do
     case Map.fetch(node.children, char) do
       :error ->
         false
 
       {:ok, child} ->
-        do_search(child, rest)
+        _search(child, rest)
     end
   end
 
