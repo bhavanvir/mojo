9f29dafc54a56f6117511bdcb56868ef4329d6e0
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-03-15 15:07:11 -0700

    Add tests

--- mojo/test/mojo_test.exs
+++ mojo/test/mojo_test.exs
@@ -2,7 +2,53 @@ defmodule MojoTest do
   use ExUnit.Case
   doctest Mojo
 
-  test "greets the world" do
-    assert Mojo.hello() == :world
+  setup _context do
+    root = Mojo.build_dict(["word"])
+    %{root: root}
+  end
+
+  test "build a dictionary", %{root: root} do
+    assert root ==
+             %Trie{
+               root: %TrieNode{
+                 children: %{
+                   "w" => %TrieNode{
+                     children: %{
+                       "o" => %TrieNode{
+                         children: %{
+                           "r" => %TrieNode{
+                             children: %{"d" => %TrieNode{children: %{}, end_of_word?: true}},
+                             end_of_word?: false
+                           }
+                         },
+                         end_of_word?: false
+                       }
+                     },
+                     end_of_word?: false
+                   }
+                 },
+                 end_of_word?: false
+               }
+             }
+  end
+
+  test "search for a valid word", %{root: root} do
+    assert Mojo.in_dict?(root, "word") == true
+  end
+
+  test "search for an invalid word", %{root: root} do
+    assert Mojo.in_dict?(root, "words") == false
+  end
+
+  test "find word with a valid prefix", %{root: root} do
+    assert Mojo.prefix_matches(root, "wo") == ["word"]
+  end
+
+  test "find word with an invalid prefix", %{root: root} do
+    assert Mojo.prefix_matches(root, "o") == []
+  end
+
+  test "find corrections for errors in passage", %{root: root} do
+    assert Mojo.parse(root, ["woords"]) == %{"woords" => ["word"]}
   end
 end
