3bc50fbe66e115cdfa5c3a1e5b9e2facc23784c1
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-03-15 14:52:19 -0700

    Add initial implementation of input parsing

--- mojo/lib/mojo.ex
+++ mojo/lib/mojo.ex
@@ -19,4 +19,13 @@ defmodule Mojo do
   def prefix_matches(root, word) do
     Trie.matches(root, word)
   end
+
+  def parse(root, passage) do
+    Enum.reduce(passage, %{}, fn word, errors ->
+      case Mojo.in_dict?(root, word) do
+        true -> errors
+        false -> Map.put(errors, word, Mojo.prefix_matches(root, word))
+      end
+    end)
+  end
 end
