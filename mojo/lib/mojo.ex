56823d6e07c6af15b6998e316261273ad9fe1ff3
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-04-03 16:13:03 +0100

    Fix doctest errors

--- mojo/lib/mojo.ex
+++ mojo/lib/mojo.ex
@@ -11,8 +11,7 @@ defmodule Mojo do
 
   ## Examples
 
-    iex> root = Mojo.create_dict()
-    iex> root
+    iex> Mojo.create_dict()
     %Trie{root: %TrieNode{children: %{}, is_terminal: false}}
 
   """
@@ -25,8 +24,9 @@ defmodule Mojo do
 
   ## Examples
 
-    iex> root = Mojo.update_dict(root, ["word1", "word2"])
-    iex> root
+    iex> dict1 = Mojo.create_dict()
+    iex> updated_dict1 = Mojo.update_dict(dict1, ["word1", "word2"])
+    iex> updated_dict1
     %Trie{
       root: %TrieNode{
         children: %{
@@ -57,8 +57,9 @@ defmodule Mojo do
       }
     }
 
-    iex> root = Mojo.update_dict(root, "word3")
-    iex> root
+    iex> dict2 = Mojo.create_dict()
+    iex> updated_dict2 = Mojo.update_dict(dict2, "word3")
+    iex> updated_dict2
     %Trie{
       root: %TrieNode{
         children: %{
@@ -105,10 +106,11 @@ defmodule Mojo do
 
   ## Examples
 
-    iex> Mojo.in_dict?(root, "word3")
+    iex> dict3 = Mojo.create_dict()
+    iex> dict3 = Mojo.update_dict(dict3, "word3")
+    iex> Mojo.in_dict?(dict3, "word3")
     true 
-
-    iex> Mojo.in_dict?(root, "word4")
+    iex> Mojo.in_dict?(dict3, "word4")
     false
   """
   def in_dict?(root, word) do
@@ -122,7 +124,9 @@ defmodule Mojo do
 
   ## Examples
     
-    iex> Mojo.prefix_matches(root, "word")
+    iex> dict4 = Mojo.create_dict()
+    iex> dict4 = Mojo.update_dict(dict4, ["word1", "word2", "word3"])
+    iex> Mojo.prefix_matches(dict4, "word")
     ["word1", "word2", "word3"]
   """
   def prefix_matches(root, prefix) do
@@ -136,8 +140,10 @@ defmodule Mojo do
 
   ## Examples
 
-    iex> Mojo.parse(root, ["werd"])
-    %{"werd" => ["word1", "word2", "word3"]
+    iex> dict5 = Mojo.create_dict()
+    iex> dict5 = Mojo.update_dict(dict5, ["word1", "word2", "word3"])
+    iex> Mojo.parse(dict5, ["werd"])
+    %{"werd" => ["word1", "word2", "word3"]}
   """
   def parse(root, passage) do
     Enum.reduce(passage, %{}, fn word, errors ->
